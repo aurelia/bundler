1d4682c360284c9a94ffc61dc5b2a52f3928ca71
Shuhel Ahmed <ahmedshuhel@newscred.com>
2016-12-17 16:02:43 +0600

    fix get module name tests

--- test/get_full_module_name.spec.ts
+++ test/get_full_module_name.spec.ts
@@ -1,45 +1,45 @@
-import  expect  from 'expect';
+import  { expect }  from 'chai';
 import { getFullModuleName } from '../lib/bundler';
 import  config  from './config';
 
 describe('getFullModuleName', () => {
   it('is defined', () => {
-    expect(getFullModuleName).toExist();
+    expect(getFullModuleName).to.exist;
   });
 
   it('config has map defined', () => {
-    expect(config.map).toExist();
+    expect(config.map).to.exist;
   });
 
   it('returns the match immediately when exact match is found', () => {
     let moduleName = 'github:aurelia/framework@0.17.0';
     let match = getFullModuleName(moduleName, config.map);
-    expect(match).toBe(moduleName);
+    expect(match).to.be.equal(moduleName);
 
     moduleName = 'font-awesome';
     match = getFullModuleName(moduleName, config.map);
-    expect(match).toBe(moduleName);
+    expect(match).to.be.equal(moduleName);
   });
 
   it('returns full module name with version when module name is specified without registry and version number', () => {
     let fullName = 'github:aurelia/framework@0.17.0';
     let match = getFullModuleName('aurelia/framework', config.map);
-    expect(match).toBe(fullName);
+    expect(match).to.be.equal(fullName);
   });
 
   it('returns configured/specified module-name/pattern itself when no match found', () => {
     let name = '[*]';
     let match = getFullModuleName(name, config.map);
-    expect(match).toBe(name);
+    expect(match).to.be.equal(name);
 
     name = 'app/**/**';
     match = getFullModuleName(name, config.map);
-    expect(match).toBe(name);
+    expect(match).to.be.equal(name);
   });
 
   it('throws error when multiple match found', () => {
     let moduleName = 'util';
     let fn = getFullModuleName.bind(undefined, moduleName, config.map);
-    expect(fn).toThrow(/A version conflict was found among the module names specified/);
+    expect(fn).to.throw(/A version conflict was found among the module names specified/);
   });
 });
